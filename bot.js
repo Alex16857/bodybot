fb27538b6ceb1be8ee60985482daf009f618156b
kunggenius123 <37816428+kunggenius123@users.noreply.github.com>
2018-06-05 13:02:24 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,9 +12,9 @@ client.on('message', message => {
           
          
            .setImage(meme[index2])
-           .setFooter("PizzaBot", "http://thecookielife.com/wp-content/uploads/2018/04/pizza-cartoon-cute-pizza-stickers-detourshirts-redbubble-space-clipart.jpg%22)
+
            .addField("Breathing Process", "Breathing starts at nose or mouth, you breath in from nose or mouth, and then goes down into the windpipe AKA trachea");
-           .addField("Aevoli", "And then goes down into the windpipe AKA trachea, trachea divides into air passage called bronchial tubes, for lungs to perform the best the airways have to be open during the act of breathing in and out also have to be free from large amounts of mucus, bronchial tubes divide into smaller things as going through lungs called alveoli, there are over 300 million alveoli in your body, the alveoli are surrounded by a mesh of tiny blood vessels called capillaries, oxygen from the inhaled air passes through the alveoli walls and into the blood");
+           .addField("Aevoli", "And then goes down into the windpipe aka trachea, trachea divides into air passage called bronchial tubes, for lungs to perform the best the airways have to be open during the act of breathing in and out also have to be free from large amounts of mucus, bronchial tubes divide into smaller things as going through lungs called alveoli, there are over 300 million alveoli in your body, the alveoli are surrounded by a mesh of tiny blood vessels called capillaries, oxygen from the inhaled air passes through the alveoli walls and into the blood");
 
         message.channel.send({embed});
   	}
